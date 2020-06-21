7f53fa73e08f558b778d379948c32a3cf7e81e31
Donatello <it.donatello@gmail.com>
2020-06-21 20:13:50 +0200

    Aggiunta introduzione del gioco

--- src/main/java/it/uniba/dlc/adventure/Engine.java
+++ src/main/java/it/uniba/dlc/adventure/Engine.java
@@ -12,58 +12,75 @@ import java.util.Scanner;
  * Motore di gioco.
  */
 public class Engine {
-    
+
     private final GameDescription game;
-    
     private final Parser parser;
-    
+
     /**
-     * 
-     * @param game 
+     * Inizializzazione del gioco.
+     *
+     * @param game
      */
     public Engine(GameDescription game) {
-        this.game = game;
-        try {
-            this.game.init();
-        } catch (Exception ex) {
-            System.err.println(ex);
-        }
-        parser = new Parser();
+
+	this.game = game;
+
+	try {
+	    this.game.intro(); // Mostra introduzione del gioco
+	    this.game.init(); // Inizializzazione del gioco
+	} catch (Exception ex) {
+	    System.err.println("Impossibile avviare il gioco.\nEccezione verificata: " + ex);
+	}
+
+	parser = new Parser();
+
     }
-    
+
     /**
-     * 
+     * Avvio del gioco.
      */
     public void run() {
-        System.out.println(game.getCurrentRoom().getName());
-        System.out.println("================================================");
-        System.out.println(game.getCurrentRoom().getDescription());
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextLine()) {
-            String command = scanner.nextLine(); // Quando premo INVIO, tutto ciò che ho scritto viene passato alla stringa "command"
-            ParserOutput p = parser.parse(command, game.getCommands(), game.getCurrentRoom().getItems(), game.getInventory());
-            if (p.getCommand() != null && p.getCommand().getType() == CommandType.END) {
-                System.out.println("Addio!");
-                break;
-            } else {
-                game.nextMove(p, System.out);
-                System.out.println("================================================");
-            }
-        }
+
+	//System.out.println(game.getCurrentRoom().getName()); // Nome della stanza attuale
+	System.out.println("================================================");
+	System.out.println(game.getCurrentRoom().getDescription()); // Descrizione della stanza attuale
+	System.out.println("================================================");
+
+	Scanner scanner = new Scanner(System.in);
+
+	while (scanner.hasNextLine()) {
+
+	    String console = scanner.nextLine(); // Quando premo INVIO, tutto ciò che ho scritto viene passato nella stringa "console"
+
+	    // Stringa inserita + comando trovato + oggetti nella stanza + inventario
+	    ParserOutput p = parser.parse(console, game.getCommands(), game.getCurrentRoom().getItems(), game.getInventory());
+
+	    if (p.getCommand() != null && p.getCommand().getType() == CommandType.END) {
+		System.out.println("Addio!");
+		break;
+	    } else {
+		game.nextMove(p, System.out);
+		System.out.println("================================================");
+	    }
+
+	} // fine "while"
+
     }
-    
+
     /**
-     * 
-     * @param args 
+     * Esecuzione del gioco.
+     *
+     * @param args
      */
     public static void main(String[] args) {
-        // Inizializzo musica gioco
-        String filepath ="song.wav";
-        Music musicObject = new Music();
-        musicObject.playMusic(filepath);
-        
-        Engine engine = new Engine(new Game());
-        engine.run();
+	// Inizializzo musica gioco
+	String filepath = "song.wav";
+	Music musicObject = new Music();
+	musicObject.playMusic(filepath);
+
+	Engine engine = new Engine(new Game());
+	
+	engine.run(); // Avvio del gioco
     }
-    
-}
+
+} // fine della classe principale "Engine"

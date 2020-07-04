d00ebb181c2b5aff66bf1a428f3c291138a89346
Luigi Vulcano <luigibros16@gmail.com>
2020-07-04 18:27:51 +0200

    Modificato Musica

--- src/main/java/it/dlc/adventure/swing/Music.java
+++ src/main/java/it/dlc/adventure/swing/Music.java
@@ -22,40 +22,43 @@ public class Music extends JDialog {
 
     public void playMusic() {
 
-	try {
-	    File musicPath = new File("src/main/resources/media/Soundtrack.wav");
+        try {
+            File musicPath = new File("src/main/resources/media/Soundtrack.wav");
 
-	    if (musicPath.exists()) {
-		AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
-		Clip clip = AudioSystem.getClip();
-		clip.open(audioInput);
+            if (musicPath.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
 
-		Object[] message = {"Vuoi attivare la musica?"};
+                Object[] message = {"Vuoi attivare la musica?"};
 
-		Object[] options = {"Si", "No"};
-		int n = JOptionPane.showOptionDialog(new JFrame(),
-			message, "",
-			JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
-			options, options[1]);
-		if (n == JOptionPane.OK_OPTION) { // Risposta affermativa
-		    clip.start();
-		    clip.loop(Clip.LOOP_CONTINUOUSLY);
-		}
-		if (n == JOptionPane.NO_OPTION) { // Risposta negativa
-		    clip.stop();
-		    clip.close();
-		}
-	    } else {
-		System.out.println("######################################################");
-		System.err.println("Impossibile riprodurre la musica, file non trovato.");
-		System.out.println("######################################################");
-	    }
+                Object[] options = {"Si", "No"};
+                JFrame jf = new JFrame();
+                jf.setAlwaysOnTop(true);
+                int selectedOption = JOptionPane.showOptionDialog(jf,
+                        message, "Impostazione Musica",
+                        JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null,
+                        options, options[1]);
 
-	} catch (HeadlessException | IOException | LineUnavailableException | UnsupportedAudioFileException ex) {
-	    System.out.println("################################################");
-	    System.err.println("Impossibile riprodurre la musica.\nEccezione verificata: " + ex);
-	    System.out.println("################################################");
-	}
+                if (selectedOption == JOptionPane.OK_OPTION) { // Risposta affermativa
+                    clip.start();
+                    clip.loop(Clip.LOOP_CONTINUOUSLY);
+                }
+                if (selectedOption == JOptionPane.NO_OPTION) { // Risposta negativa
+                    clip.stop();
+                    clip.close();
+                }
+            } else {
+                System.out.println("######################################################");
+                System.err.println("Impossibile riprodurre la musica, file non trovato.");
+                System.out.println("######################################################");
+            }
+
+        } catch (HeadlessException | IOException | LineUnavailableException | UnsupportedAudioFileException ex) {
+            System.out.println("################################################");
+            System.err.println("Impossibile riprodurre la musica.\nEccezione verificata: " + ex);
+            System.out.println("################################################");
+        }
 
     }
 
